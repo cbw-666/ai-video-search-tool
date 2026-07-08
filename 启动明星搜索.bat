62f56690280fc665dc0caeeab37a7bd81f05df74
NeoDev <72789488+cbw-666@users.noreply.github.com>
2026-07-08 17:14:04 +0800

    Update 启动明星搜索.bat

--- 启动明星搜索.bat
+++ 启动明星搜索.bat
@@ -5,6 +5,7 @@ echo      AI Star Search Engine Initializing...
 echo ==========================================
 
 set HF_HOME=%~dp0models_cache
+set HF_ENDPOINT=https://hf-mirror.com
 set INSIGHTFACE_HOME=%~dp0.insightface
 
 "%~dp0venv\Scripts\python.exe" -m streamlit run star_search.py --server.headless false
